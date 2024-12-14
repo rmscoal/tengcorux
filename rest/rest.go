43ae591e5d57e1c9fb8ce85051d6a6b8ba2510eb
Rifky Manuel Satyana <101387958+rmscoal@users.noreply.github.com>
2024-12-14 10:34:24 +0700

    fix(rest): better span attributes and clearer span name (#13)

--- rest/rest.go
+++ rest/rest.go
@@ -2,12 +2,16 @@ package rest
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"reflect"
+	"time"
+
 	"github.com/go-resty/resty/v2"
 	"github.com/google/uuid"
 	"github.com/rmscoal/tengcorux/tracer"
 	"github.com/rmscoal/tengcorux/tracer/attribute"
-	"time"
 )
 
 const XRequestId = "X-Request-Id"
@@ -36,95 +40,162 @@ func New(opts ...Option) *Rest {
 	return rest
 }
 
+func Version() string {
+	return "v0.1.1"
+}
+
 // registerTracerMiddleware registers OnBeforeRequest and OnAfterResponse middleware
 // for tracer to start the span and captures the attributes.
 func (r *Rest) registerTracerMiddleware() {
 	r.Client = r.Client.
-		OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
-			// Here, we are going to start a span. We should also fill the span
-			// with attributes. Therefore, before resty makes a request, we need
-			// to capture the following for tracer:
-			// 1. Method (like GET, POST, and others),
-			// 2. URL Target, this should include path and query params,
-			// 3. Body of the payload if it is a POST/PUT/PATCH request, and
-			// 4. Headers of the request.
-			method := request.Method
-			ctx, span := tracer.StartSpan(request.Context(), method,
-				tracer.WithSpanType(tracer.SpanTypeExit),
-				tracer.WithSpanLayer(tracer.SpanLayerHttp),
-			)
+		OnBeforeRequest(
+			func(client *resty.Client, request *resty.Request) error {
+				// Here, we are going to start a span. We should also fill the span
+				// with attributes. Therefore, before resty makes a request, we need
+				// to capture the following for tracer:
+				// 1. Method (like GET, POST, and others),
+				// 2. URL Target, this should include path and query params,
+				// 3. Body of the payload if it is a POST/PUT/PATCH request, and
+				// 4. Headers of the request.
+				method := request.Method
+				ctx, span := tracer.StartSpan(
+					request.Context(), "HTTP "+method+" Request",
+					tracer.WithSpanType(tracer.SpanTypeExit),
+					tracer.WithSpanLayer(tracer.SpanLayerHttp),
+				)
 
-			// Injects X-Request-Id to the request by reading from the context
-			requestID, found := ctx.Value(XRequestId).(string)
-			if !found {
-				requestID = fmt.Sprintf("%s-%s", uuid.New().String(), time.Now().Format(time.RFC3339))
-				ctx = context.WithValue(ctx, XRequestId, requestID)
-			}
-			request.SetHeader(XRequestId, requestID)
+				// Injects X-Request-Id to the request by reading from the context
+				requestID, found := ctx.Value(XRequestId).(string)
+				if !found {
+					requestID = fmt.Sprintf(
+						"%s-%s", uuid.New().String(),
+						time.Now().Format(time.RFC3339),
+					)
+					ctx = context.WithValue(ctx, XRequestId, requestID)
+				}
+				request.SetHeader(XRequestId, requestID)
 
-			// Add spans to the attribute
-			span.SetAttributes(
-				attribute.HTTPRequestID(requestID),
-				attribute.HTTPRequestMethod(method),
-				attribute.KeyValuePair("http.request.headers", request.Header),
-			)
-			if method == resty.MethodPost ||
-				method == resty.MethodPut ||
-				method == resty.MethodPatch {
-				span.SetAttributes(attribute.HTTPRequestBody(request.Body))
-			}
+				// Add spans to the attribute
+				span.SetAttributes(
+					attribute.HTTPRequestID(requestID),
+					attribute.HTTPRequestMethod(method),
+					attribute.KeyValuePair(
+						"http.request.headers",
+						generateHeaderAttribute(request.Header),
+					),
+				)
 
-			request.SetContext(ctx)
-			return nil
-		}).
-		OnError(func(request *resty.Request, err error) {
-			// OnError should be triggered when resty failed to make a request.
-			// Thus, the tracer should mark the current span as error.
-			span := tracer.SpanFromContext(request.Context())
-			if span == nil {
-				// Ignore if there are no span.
-				return
-			}
-			defer span.End()
+				if method == resty.MethodPost ||
+					method == resty.MethodPut ||
+					method == resty.MethodPatch {
+					span.SetAttributes(
+						attribute.HTTPRequestBody(generateBodyAttribute(request.Body)),
+					)
+				}
 
-			span.SetAttributes(attribute.HTTPUrl(request.URL))
-			span.RecordError(err)
-		}).
-		OnPanic(func(request *resty.Request, err error) {
-			// OnPanic should be triggered after resty makes a request. Marks
-			// the span as failed and record error.
-			span := tracer.SpanFromContext(request.Context())
-			if span == nil {
-				// Ignore if there are no span.
-				return
-			}
-			defer span.End()
-
-			span.SetAttributes(attribute.HTTPUrl(request.URL))
-			span.RecordError(err)
-		}).
-		OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
-			// Here we are going to get the span from the request's context.
-			// After request was made, we need to capture the following attributes:
-			// 1. The request body if there are any,
-			// 2. The response status code, and
-			// 3. The response header.
-			span := tracer.SpanFromContext(response.Request.Context())
-			if span == nil {
-				// Ignore if there are no span.
+				request.SetContext(ctx)
 				return nil
-			}
-			defer span.End()
+			},
+		).
+		OnError(
+			func(request *resty.Request, err error) {
+				// OnError should be triggered when resty failed to make a request.
+				// Thus, the tracer should mark the current span as error.
+				span := tracer.SpanFromContext(request.Context())
+				if span == nil {
+					// Ignore if there are no span.
+					return
+				}
+				defer span.End()
 
-			span.SetAttributes(
-				attribute.HTTPResponseStatus(response.StatusCode()),
-				attribute.HTTPUrl(response.Request.URL),
-				attribute.KeyValuePair("http.response.headers", response.Header),
-			)
-			if response.Body() != nil {
-				span.SetAttributes(attribute.HTTPResponseBody(string(response.Body())))
-			}
+				span.SetAttributes(attribute.HTTPUrl(request.URL))
+				span.RecordError(err)
+			},
+		).
+		OnPanic(
+			func(request *resty.Request, err error) {
+				// OnPanic should be triggered after resty makes a request. Marks
+				// the span as failed and record error.
+				span := tracer.SpanFromContext(request.Context())
+				if span == nil {
+					// Ignore if there are no span.
+					return
+				}
+				defer span.End()
 
-			return nil
-		})
+				span.SetAttributes(attribute.HTTPUrl(request.URL))
+				span.RecordError(err)
+			},
+		).
+		OnAfterResponse(
+			func(client *resty.Client, response *resty.Response) error {
+				// Here we are going to get the span from the request's context.
+				// After request was made, we need to capture the following attributes:
+				// 1. The request body if there are any,
+				// 2. The response status code, and
+				// 3. The response header.
+				span := tracer.SpanFromContext(response.Request.Context())
+				if span == nil {
+					// Ignore if there are no span.
+					return nil
+				}
+				defer span.End()
+
+				span.SetAttributes(
+					attribute.HTTPResponseStatus(response.StatusCode()),
+					attribute.HTTPUrl(response.Request.URL),
+					attribute.KeyValuePair(
+						"http.response.headers",
+						generateHeaderAttribute(response.Header()),
+					),
+				)
+
+				if response.Body() != nil {
+					span.SetAttributes(
+						attribute.HTTPResponseBody(
+							generateBodyAttribute(response.Body()),
+						),
+					)
+				}
+
+				return nil
+			},
+		)
+}
+
+func generateBodyAttribute(body any) any {
+	if body == nil {
+		return nil
+	}
+
+	typeof := reflect.TypeOf(body)
+	switch typeof.Kind() {
+	case reflect.Map, reflect.Struct:
+		b, err := json.MarshalIndent(body, "", "  ")
+		if err != nil {
+			return body
+		}
+		return string(b)
+	case reflect.Slice:
+		if b, ok := body.([]byte); ok {
+			return string(b)
+		} else {
+			return fmt.Sprintf("%v", body)
+		}
+	default:
+		return body
+	}
+}
+
+func generateHeaderAttribute(header http.Header) any {
+	if header == nil {
+		return nil
+	}
+
+	b, err := json.MarshalIndent(header, "", "  ")
+	if err != nil {
+		return header
+	}
+
+	return string(b)
 }
--- rest/rest_test.go
+++ rest/rest_test.go
@@ -2,13 +2,15 @@ package rest
 
 import (
 	"context"
-	"github.com/rmscoal/tengcorux/tracer"
-	"github.com/rmscoal/tengcorux/tracer/attribute"
-	"github.com/rmscoal/tengcorux/tracer/tracetest"
+	"encoding/json"
 	"net/http"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/rmscoal/tengcorux/tracer"
+	"github.com/rmscoal/tengcorux/tracer/attribute"
+	"github.com/rmscoal/tengcorux/tracer/tracetest"
 )
 
 func TestRest_New(t *testing.T) {
@@ -23,239 +25,422 @@ func TestRest_New(t *testing.T) {
 		_ = server.ListenAndServe()
 	}()
 
-	t.Run("NoOption", func(t *testing.T) {
-		rest := New().SetBaseURL("http://localhost:8123")
-		if rest == nil {
-			t.Fatal("rest should not be nil")
-		}
-
-		t.Run("Hit Get /success", func(t *testing.T) {
-			resp, err := rest.R().
-				SetContext(context.Background()).
-				Get("/success")
-			if err != nil {
-				t.Fatalf("error should be nil, got %v", err)
-			} else if string(resp.Body()) != "Success" {
-				t.Fatalf("body should be Success, got %v", string(resp.Body()))
-			} else if resp.StatusCode() != http.StatusOK {
-				t.Fatal("status code should be 200")
-			}
-		})
-
-		t.Run("Hit Post /success", func(t *testing.T) {
-			resp, err := rest.R().SetContext(context.Background()).Post("/success")
-			if err != nil {
-				t.Fatalf("error should be nil, got %v", err)
-			} else if string(resp.Body()) != "Success" {
-				t.Fatalf("body should be Success, got %v", string(resp.Body()))
-			} else if resp.StatusCode() != http.StatusOK {
-				t.Fatal("status code should be 200")
-			}
-		})
-
-	})
-
-	t.Run("WithTracerEnabled", func(t *testing.T) {
-		t.Run("Hit Get /success", func(t *testing.T) {
-			tr := tracetest.NewTracer()
-			tracer.SetGlobalTracer(tr)
-			rest := New(WithTracerEnabled()).SetBaseURL("http://localhost:8123")
+	t.Run(
+		"NoOption", func(t *testing.T) {
+			rest := New().SetBaseURL("http://localhost:8123")
 			if rest == nil {
 				t.Fatal("rest should not be nil")
 			}
 
-			resp, err := rest.R().SetContext(context.Background()).Get("/success")
-			if err != nil {
-				t.Fatalf("error should be nil, got %v", err)
-			} else if string(resp.Body()) != "Success" {
-				t.Fatalf("body should be Success, got %v", string(resp.Body()))
-			} else if resp.StatusCode() != http.StatusOK {
-				t.Fatal("status code should be 200")
+			t.Run(
+				"Hit Get /success", func(t *testing.T) {
+					resp, err := rest.R().
+						SetContext(context.Background()).
+						Get("/success")
+					if err != nil {
+						t.Fatalf("error should be nil, got %v", err)
+					} else if string(resp.Body()) != "Success" {
+						t.Fatalf(
+							"body should be Success, got %v",
+							string(resp.Body()),
+						)
+					} else if resp.StatusCode() != http.StatusOK {
+						t.Fatal("status code should be 200")
+					}
+				},
+			)
+
+			t.Run(
+				"Hit Post /success", func(t *testing.T) {
+					resp, err := rest.R().SetContext(context.Background()).Post("/success")
+					if err != nil {
+						t.Fatalf("error should be nil, got %v", err)
+					} else if string(resp.Body()) != "Success" {
+						t.Fatalf(
+							"body should be Success, got %v",
+							string(resp.Body()),
+						)
+					} else if resp.StatusCode() != http.StatusOK {
+						t.Fatal("status code should be 200")
+					}
+				},
+			)
+
+		},
+	)
+
+	t.Run(
+		"WithTracerEnabled", func(t *testing.T) {
+			t.Run(
+				"Hit Get /success", func(t *testing.T) {
+					tr := tracetest.NewTracer()
+					tracer.SetGlobalTracer(tr)
+					rest := New(WithTracerEnabled()).SetBaseURL("http://localhost:8123")
+					if rest == nil {
+						t.Fatal("rest should not be nil")
+					}
+
+					resp, err := rest.R().SetContext(context.Background()).Get("/success")
+					if err != nil {
+						t.Fatalf("error should be nil, got %v", err)
+					} else if string(resp.Body()) != "Success" {
+						t.Fatalf(
+							"body should be Success, got %v",
+							string(resp.Body()),
+						)
+					} else if resp.StatusCode() != http.StatusOK {
+						t.Fatal("status code should be 200")
+					}
+
+					// Check the tracer
+					ended := tr.Recorder().EndedSpans()
+					if len(ended) != 1 {
+						t.Fatalf("ended spans should be 1, got %v", len(ended))
+					}
+					span := ended[0]
+					if span.Type != tracer.SpanTypeExit {
+						t.Fatalf(
+							"span.Type should be %d, got %d",
+							tracer.SpanTypeExit, span.Type,
+						)
+					} else if span.Layer != tracer.SpanLayerHttp {
+						t.Fatalf(
+							"layer should be %d, got %d", tracer.SpanLayerHttp,
+							span.Layer,
+						)
+					}
+
+					for _, attr := range span.Attributes {
+						switch attr.Key {
+						case attribute.HTTPRequestIDKey:
+							if attr.Value == "" {
+								t.Fatalf("the request id span attribute should not be empty")
+							}
+						case attribute.HTTPRequestMethodKey:
+							if attr.Value != "GET" {
+								t.Fatalf(
+									"http request method should be GET, got %v",
+									attr.Value,
+								)
+							}
+						case "http.request.headers":
+							if attr.Value == nil {
+								t.Fatalf("http request headers should not be nil")
+							}
+						case attribute.HTTPUrlKey:
+							if attr.Value != "http://localhost:8123/success" {
+								t.Fatalf(
+									"http url should be http://localhost:8123/success, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.HTTPResponseStatusKey:
+							if attr.Value != 200 {
+								t.Fatalf(
+									"http response status should be 200, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.HTTPResponseBodyKey:
+							if attr.Value != "Success" {
+								t.Fatalf(
+									"http response body should be Success, got %v",
+									attr.Value,
+								)
+							}
+						}
+					}
+				},
+			)
+
+			t.Run(
+				"Hit Get /error", func(t *testing.T) {
+					tr := tracetest.NewTracer()
+					tracer.SetGlobalTracer(tr)
+					rest := New(WithTracerEnabled()).SetBaseURL("http://localhost:8123")
+					if rest == nil {
+						t.Fatal("rest should not be nil")
+					}
+
+					resp, err := rest.R().SetContext(context.Background()).Get("/error")
+					if err != nil {
+						t.Fatalf("error should be nil, got %v", err)
+					} else if strings.Trim(
+						string(resp.Body()), " ",
+					) != "Bad Request" {
+						t.Fatalf(
+							"body should be Bad Request, got %v",
+							string(resp.Body()),
+						)
+					} else if resp.StatusCode() != http.StatusBadRequest {
+						t.Fatal("status code should be 400")
+					}
+
+					// Check the tracer
+					ended := tr.Recorder().EndedSpans()
+					if len(ended) != 1 {
+						t.Fatalf("ended spans should be 1, got %v", len(ended))
+					}
+					span := ended[0]
+					if span.Type != tracer.SpanTypeExit {
+						t.Fatalf(
+							"span.Type should be %d, got %d",
+							tracer.SpanTypeExit, span.Type,
+						)
+					} else if span.Layer != tracer.SpanLayerHttp {
+						t.Fatalf(
+							"layer should be %d, got %d", tracer.SpanLayerHttp,
+							span.Layer,
+						)
+					}
+
+					for _, attr := range span.Attributes {
+						switch attr.Key {
+						case attribute.HTTPRequestIDKey:
+							if attr.Value == "" {
+								t.Fatalf("the request id span attribute should not be empty")
+							}
+						case attribute.HTTPRequestMethodKey:
+							if attr.Value != "GET" {
+								t.Fatalf(
+									"http request method should be GET, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.Key("http.request.headers"):
+							if attr.Value == nil {
+								t.Fatalf("http request headers should not be nil")
+							}
+						case attribute.HTTPUrlKey:
+							if attr.Value != "http://localhost:8123/error" {
+								t.Fatalf(
+									"http url should be http://localhost:8123/error, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.HTTPResponseStatusKey:
+							if attr.Value != 400 {
+								t.Fatalf(
+									"http response status should be 400, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.HTTPResponseBodyKey:
+							if attr.Value != "Bad Request" {
+								t.Fatalf(
+									"http response body should be Bad Request, got %v",
+									attr.Value,
+								)
+							}
+						}
+					}
+				},
+			)
+
+			t.Run(
+				"Hit Post /error", func(t *testing.T) {
+					tr := tracetest.NewTracer()
+					tracer.SetGlobalTracer(tr)
+					rest := New(WithTracerEnabled()).SetBaseURL("http://localhost:8123")
+					if rest == nil {
+						t.Fatal("rest should not be nil")
+					}
+
+					body := map[string]interface{}{
+						"hello": "world",
+					}
+					resp, err := rest.R().
+						SetContext(context.Background()).
+						SetBody(body).
+						Post("/error")
+					if err != nil {
+						t.Fatalf("error should be nil, got %v", err)
+					} else if strings.Trim(
+						string(resp.Body()), " ",
+					) != "Bad Request" {
+						t.Fatalf(
+							"body should be Bad Request, got %v",
+							string(resp.Body()),
+						)
+					} else if resp.StatusCode() != http.StatusBadRequest {
+						t.Fatal("status code should be 400")
+					}
+
+					// Check the tracer
+					ended := tr.Recorder().EndedSpans()
+					if len(ended) != 1 {
+						t.Fatalf("ended spans should be 1, got %v", len(ended))
+					}
+					span := ended[0]
+					if span.Type != tracer.SpanTypeExit {
+						t.Fatalf(
+							"span.Type should be %d, got %d",
+							tracer.SpanTypeExit, span.Type,
+						)
+					} else if span.Layer != tracer.SpanLayerHttp {
+						t.Fatalf(
+							"layer should be %d, got %d", tracer.SpanLayerHttp,
+							span.Layer,
+						)
+					}
+
+					for _, attr := range span.Attributes {
+						switch attr.Key {
+						case attribute.HTTPRequestIDKey:
+							if attr.Value == "" {
+								t.Fatalf("the request id span attribute should not be empty")
+							}
+						case attribute.HTTPRequestMethodKey:
+							if attr.Value != "POST" {
+								t.Fatalf(
+									"http request method should be POST, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.Key("http.request.headers"):
+							if attr.Value == nil {
+								t.Fatalf("http request headers should not be nil")
+							}
+						case attribute.HTTPUrlKey:
+							if attr.Value != "http://localhost:8123/error" {
+								t.Fatalf(
+									"http url should be http://localhost:8123/error, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.HTTPRequestBodyKey:
+							if attr.Value == nil {
+								t.Fatalf("http request body should not be nil")
+							}
+						case attribute.HTTPResponseStatusKey:
+							if attr.Value != 400 {
+								t.Fatalf(
+									"http response status should be 400, got %v",
+									attr.Value,
+								)
+							}
+						case attribute.HTTPResponseBodyKey:
+							if attr.Value != "Bad Request" {
+								t.Fatalf(
+									"http response body should be Bad Request, got %v",
+									attr.Value,
+								)
+							}
+						}
+					}
+				},
+			)
+		},
+	)
+}
+
+func TestGenerateBodyAttribute(t *testing.T) {
+	t.Run(
+		"BodyIsByte", func(t *testing.T) {
+			res := generateBodyAttribute([]byte(`{"hello": "world"}`))
+			expected := `{"hello": "world"}`
+			if res != expected {
+				t.Errorf("expected %v but got %v", expected, res)
+			}
+			t.Log("GenerateBodyAttribute Result:", res)
+		},
+	)
+
+	t.Run(
+		"BodyIsStruct", func(t *testing.T) {
+			type req struct {
+				Username string `json:"username"`
+				Password string `json:"password"`
 			}
 
-			// Check the tracer
-			ended := tr.Recorder().EndedSpans()
-			if len(ended) != 1 {
-				t.Fatalf("ended spans should be 1, got %v", len(ended))
-			}
-			span := ended[0]
-			if span.Type != tracer.SpanTypeExit {
-				t.Fatalf("span.Type should be %d, got %d", tracer.SpanTypeExit, span.Type)
-			} else if span.Layer != tracer.SpanLayerHttp {
-				t.Fatalf("layer should be %d, got %d", tracer.SpanLayerHttp, span.Layer)
+			body := req{
+				Username: "test",
+				Password: "test",
 			}
 
-			for _, attr := range span.Attributes {
-				switch attr.Key {
-				case attribute.HTTPRequestIDKey:
-					if attr.Value == "" {
-						t.Fatalf("the request id span attribute should not be empty")
-					}
-				case attribute.HTTPRequestMethodKey:
-					if attr.Value != "GET" {
-						t.Fatalf("http request method should be GET, got %v", attr.Value)
-					}
-				case attribute.Key("http.request.headers"):
-					if attr.Value == nil {
-						t.Fatalf("http request headers should not be nil")
-					}
-				case attribute.HTTPUrlKey:
-					if attr.Value != "http://localhost:8123/success" {
-						t.Fatalf("http url should be http://localhost:8123/success, got %v", attr.Value)
-					}
-				case attribute.HTTPResponseStatusKey:
-					if attr.Value != 200 {
-						t.Fatalf("http response status should be 200, got %v", attr.Value)
-					}
-				case attribute.HTTPResponseBodyKey:
-					if attr.Value != "Success" {
-						t.Fatalf("http response body should be Success, got %v", attr.Value)
-					}
-				}
-			}
-		})
+			marshalled, _ := json.MarshalIndent(body, "", "  ")
+			expected := string(marshalled)
 
-		t.Run("Hit Get /error", func(t *testing.T) {
-			tr := tracetest.NewTracer()
-			tracer.SetGlobalTracer(tr)
-			rest := New(WithTracerEnabled()).SetBaseURL("http://localhost:8123")
-			if rest == nil {
-				t.Fatal("rest should not be nil")
+			res := generateBodyAttribute(body)
+			if res != expected {
+				t.Errorf("expected %v but got %v", expected, res)
 			}
+			t.Log("GenerateBodyAttribute Result:", res)
+		},
+	)
 
-			resp, err := rest.R().SetContext(context.Background()).Get("/error")
-			if err != nil {
-				t.Fatalf("error should be nil, got %v", err)
-			} else if strings.Trim(string(resp.Body()), " ") != "Bad Request" {
-				t.Fatalf("body should be Bad Request, got %v", string(resp.Body()))
-			} else if resp.StatusCode() != http.StatusBadRequest {
-				t.Fatal("status code should be 400")
-			}
-
-			// Check the tracer
-			ended := tr.Recorder().EndedSpans()
-			if len(ended) != 1 {
-				t.Fatalf("ended spans should be 1, got %v", len(ended))
-			}
-			span := ended[0]
-			if span.Type != tracer.SpanTypeExit {
-				t.Fatalf("span.Type should be %d, got %d", tracer.SpanTypeExit, span.Type)
-			} else if span.Layer != tracer.SpanLayerHttp {
-				t.Fatalf("layer should be %d, got %d", tracer.SpanLayerHttp, span.Layer)
-			}
-
-			for _, attr := range span.Attributes {
-				switch attr.Key {
-				case attribute.HTTPRequestIDKey:
-					if attr.Value == "" {
-						t.Fatalf("the request id span attribute should not be empty")
-					}
-				case attribute.HTTPRequestMethodKey:
-					if attr.Value != "GET" {
-						t.Fatalf("http request method should be GET, got %v", attr.Value)
-					}
-				case attribute.Key("http.request.headers"):
-					if attr.Value == nil {
-						t.Fatalf("http request headers should not be nil")
-					}
-				case attribute.HTTPUrlKey:
-					if attr.Value != "http://localhost:8123/error" {
-						t.Fatalf("http url should be http://localhost:8123/error, got %v", attr.Value)
-					}
-				case attribute.HTTPResponseStatusKey:
-					if attr.Value != 400 {
-						t.Fatalf("http response status should be 400, got %v", attr.Value)
-					}
-				case attribute.HTTPResponseBodyKey:
-					if attr.Value != "Bad Request" {
-						t.Fatalf("http response body should be Bad Request, got %v", attr.Value)
-					}
-				}
-			}
-		})
-
-		t.Run("Hit Post /error", func(t *testing.T) {
-			tr := tracetest.NewTracer()
-			tracer.SetGlobalTracer(tr)
-			rest := New(WithTracerEnabled()).SetBaseURL("http://localhost:8123")
-			if rest == nil {
-				t.Fatal("rest should not be nil")
-			}
-
+	t.Run(
+		"BodyIsMapAny", func(t *testing.T) {
 			body := map[string]interface{}{
-				"hello": "world",
-			}
-			resp, err := rest.R().
-				SetContext(context.Background()).
-				SetBody(body).
-				Post("/error")
-			if err != nil {
-				t.Fatalf("error should be nil, got %v", err)
-			} else if strings.Trim(string(resp.Body()), " ") != "Bad Request" {
-				t.Fatalf("body should be Bad Request, got %v", string(resp.Body()))
-			} else if resp.StatusCode() != http.StatusBadRequest {
-				t.Fatal("status code should be 400")
+				"username": "test",
+				"password": 1,
 			}
 
-			// Check the tracer
-			ended := tr.Recorder().EndedSpans()
-			if len(ended) != 1 {
-				t.Fatalf("ended spans should be 1, got %v", len(ended))
+			marshalled, _ := json.MarshalIndent(body, "", "  ")
+			expected := string(marshalled)
+
+			res := generateBodyAttribute(body)
+			if res != expected {
+				t.Errorf("expected %v but got %v", expected, res)
 			}
-			span := ended[0]
-			if span.Type != tracer.SpanTypeExit {
-				t.Fatalf("span.Type should be %d, got %d", tracer.SpanTypeExit, span.Type)
-			} else if span.Layer != tracer.SpanLayerHttp {
-				t.Fatalf("layer should be %d, got %d", tracer.SpanLayerHttp, span.Layer)
+			t.Log("GenerateBodyAttribute Result:", res)
+		},
+	)
+	t.Run(
+		"BodyIsMapString", func(t *testing.T) {
+			body := map[string]string{
+				"username": "test",
+				"password": "test",
 			}
 
-			for _, attr := range span.Attributes {
-				switch attr.Key {
-				case attribute.HTTPRequestIDKey:
-					if attr.Value == "" {
-						t.Fatalf("the request id span attribute should not be empty")
-					}
-				case attribute.HTTPRequestMethodKey:
-					if attr.Value != "POST" {
-						t.Fatalf("http request method should be POST, got %v", attr.Value)
-					}
-				case attribute.Key("http.request.headers"):
-					if attr.Value == nil {
-						t.Fatalf("http request headers should not be nil")
-					}
-				case attribute.HTTPUrlKey:
-					if attr.Value != "http://localhost:8123/error" {
-						t.Fatalf("http url should be http://localhost:8123/error, got %v", attr.Value)
-					}
-				case attribute.HTTPRequestBodyKey:
-					asMap := attr.Value.(map[string]interface{})
-					if asMap["hello"] != "world" {
-						t.Errorf("http request body should be %v, got %v", body, attr.Value)
-					}
-				case attribute.HTTPResponseStatusKey:
-					if attr.Value != 400 {
-						t.Fatalf("http response status should be 400, got %v", attr.Value)
-					}
-				case attribute.HTTPResponseBodyKey:
-					if attr.Value != "Bad Request" {
-						t.Fatalf("http response body should be Bad Request, got %v", attr.Value)
-					}
-				}
+			marshalled, _ := json.MarshalIndent(body, "", "  ")
+			expected := string(marshalled)
+
+			res := generateBodyAttribute(body)
+			if res != expected {
+				t.Errorf("expected %v but got %v", expected, res)
 			}
-		})
-	})
+			t.Log("GenerateBodyAttribute Result:", res)
+		},
+	)
+}
+
+func TestGenerateHeaderAttribute(t *testing.T) {
+	head := http.Header{}
+	head.Set("Content-Type", "application/x-www-form-urlencoded")
+	head.Set("X-Request-ID", "some_request_id")
+	head.Set("Topic", "RestPackage")
+
+	res := generateHeaderAttribute(head)
+
+	expected, _ := json.MarshalIndent(head, "", "  ")
+
+	if res != string(expected) {
+		t.Errorf("expected %v but got %v", expected, res)
+	}
+	t.Log("GenerateHeaderAttribute Result:", res)
+}
+
+func TestVersion(t *testing.T) {
+	if Version() != "v0.1.1" {
+		t.Fatal("incompatible version")
+	}
 }
 
 func testServer() *http.Server {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/success", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("Success"))
-	})
-	mux.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte("Bad Request"))
-	})
+	mux.HandleFunc(
+		"/success", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte("Success"))
+		},
+	)
+	mux.HandleFunc(
+		"/error", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("Bad Request"))
+		},
+	)
 
 	server := &http.Server{Handler: mux, Addr: ":8123"}
 	return server
