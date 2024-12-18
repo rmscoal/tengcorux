7741f6c55982b08c1c4d17b3a16c21b3fbfbc625
Rifky Manuel Satyana <101387958+rmscoal@users.noreply.github.com>
2024-12-18 16:01:12 +0700

    feat(gorm_tracing): adds span name generator functionality (#14)
    * feat(gorm_tracing): adds span name generator functionality
    
    * test: check err from registering plugin
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- gorm/plugin/tracing/option.go
+++ gorm/plugin/tracing/option.go
@@ -4,6 +4,14 @@ import "github.com/rmscoal/tengcorux/tracer"
 
 type Option func(*tracing)
 
+func WithSpanNameGenerator(f SpanNameGenerator) Option {
+	return func(t *tracing) {
+		if f != nil {
+			t.spanNameGenerator = f
+		}
+	}
+}
+
 // WithTracer registers the given tracer to the tracing instance.
 func WithTracer(tracer tracer.Tracer) Option {
 	return func(t *tracing) {
